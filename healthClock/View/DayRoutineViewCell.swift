8b6acf768a4de79edd966c0bfc5b442c7e6f59af
sungyoungPark <sygitter1491012@gmail.com>
2021-12-11 19:07:08 +0900

    테이블 뷰 구성

---
+++ healthClock/View/DayRoutineViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DayRoutineViewCell.swift
+//  healthClock
+//
+//  Created by 박성영 on 2021/12/11.
+//
+
+import UIKit
+
+class DayRoutineViewCell: UITableViewCell {
+
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
---
+++ healthClock/View/RoutineViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  healthClock
+//
+//  Created by 박성영 on 2021/12/11.
+//
+
+import UIKit
+import SnapKit
+
+class RoutineViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let tableView : UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .white
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(DayRoutineViewCell.classForCoder(), forCellReuseIdentifier: "DayRoutine")
+        
+    }
+
+    func setUI(){
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.topItem?.title = "Routine"
+        
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { (maker) in
+            maker.top.bottom.leading.trailing.equalTo(0)
+        }
+        
+        view.backgroundColor = .white
+    }
+    
+
+}
+
+extension RoutineViewController {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+}
